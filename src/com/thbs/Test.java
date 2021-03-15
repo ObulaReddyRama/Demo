a91890c74e51415972a4d546abc1392008d68cc9
RAMA_OBULA_reddy <Rama_Obula_reddy@Dellteam.com>
2021-03-15 23:18:33 +0530

    project created

---
+++ src/com/thbs/Test.java
@@ -0,0 +1,21 @@
+package com.thbs;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("welcome to Git");
+		m(10);
+		print("rama");
+
+	}
+
+	private static void print(String string) {
+		System.out.println(string);
+	}
+
+	private static void m(int i) {
+		System.out.println(i);
+	}
+
+}
